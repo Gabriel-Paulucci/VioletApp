fb5501b9a22fded311742aef791483b596c221de
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2022-03-28 15:57:55 -0300

    Add: form input

---
+++ src/components/form/FormInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <input
+    :value="modelValue"
+    class="my-2 px-2 rounded border-2"
+    v-bind:class="{ 'border-red-700': error }"
+    v-bind:placeholder="placeholder"
+    v-bind:type="type"
+    @input="updateModelValue"
+  />
+</template>
+
+<script setup lang="ts">
+type InputType = "text" | "email" | "password";
+
+defineProps<{
+  modelValue: string;
+  placeholder: string;
+  type: InputType;
+  error?: boolean;
+}>();
+
+const emits = defineEmits(["update:modelValue"]);
+
+function updateModelValue(value: Event) {
+  emits("update:modelValue", (value.target as HTMLInputElement).value);
+}
+</script>
