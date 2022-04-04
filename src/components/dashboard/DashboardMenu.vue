46ab219c95e7ddf5b19adcd78ad8a5326ffa5026
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2022-04-03 21:13:48 -0300

    Add: dashboard template

---
+++ src/components/dashboard/DashboardMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col h-full">
+    <div class="grow-0">
+      <span class="text-center block text-2xl my-2 text-emerald-200 font-bold"
+        >Violet App</span
+      >
+    </div>
+    <div class="grow">
+      <button class="my-1 mx-auto block" @click="$emit('myApps')">
+        My App
+      </button>
+    </div>
+    <div class="grow-0">
+      <button class="my-1 mx-auto block">Logout</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+
+const emit = defineEmits<{
+  (event: "myApps"): void;
+}>();
+</script>
---
+++ src/components/dashboard/DashboardTemplate.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="h-screen w-screen flex flex-row">
+    <div class="w-[200px] bg-emerald-700">
+      <DashboardMenu @my-apps="myApps" />
+    </div>
+    <div class="grow bg-teal-50">
+      <DashboardContainer>
+        <slot></slot>
+      </DashboardContainer>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import DashboardMenu from "@/components/dashboard/DashboardMenu.vue";
+import DashboardContainer from "@/components/dashboard/DashboardContainer.vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+function myApps() {
+  router.push("/apps");
+}
+</script>
